8083edb14a9881ba7d7ae7e2ee4cba6e22ed0f20
KristinaBandic <kristinakiketina@gmail.com>
2025-03-27 14:04:13 +0100

    implemented DAL layer and create database with EF

---
+++ DAL/Migrations/Configuration.cs
@@ -0,0 +1,31 @@
+﻿namespace DAL.Migrations
+{
+    using DAL.Entities;
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<DAL.MyContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+        }
+
+        protected override void Seed(DAL.MyContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+
+            var subject = new SubjectSchool();
+            subject.SubjectSchoolId = 1;
+            subject.Name = "hrvatski";
+            context.SubjectSchools.Add(subject);
+
+            base.Seed(context);
+        }
+    }
+}
---
+++ DAL/MyContext.cs
@@ -0,0 +1,27 @@
+﻿using DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class MyContext:DbContext
+    {
+        public MyContext() : base("STSDB")
+        {
+            Database.SetInitializer(new MigrateDatabaseToLatestVersion<MyContext, DAL.Migrations.Configuration>("STSDB"));
+        }
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Teacher> Teachers { get; set; }
+        public DbSet<SubjectSchool> SubjectSchools { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
