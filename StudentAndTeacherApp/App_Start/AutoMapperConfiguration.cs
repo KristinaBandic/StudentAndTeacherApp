7515f1aff4bfe87072afe5eb13b492c8c8967643
KristinaBandic <kristinakiketina@gmail.com>
2025-06-03 13:10:58 +0200

    Added autoMapper and autofac configuration

---
+++ StudentAndTeacherApp/App_Start/AutoMapperConfiguration.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using Model.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using static StudentAndTeacherApp.Controllers.SubjectSchoolController;
+
+namespace StudentAndTeacherApp.App_Start
+{
+    public class AutoMapperConfiguration
+    {
+        public static void Init()
+        {
+            MapperConfiguration = new MapperConfiguration(ctf =>
+            {
+                ctf.CreateMap<ISubjectSchoolDomain, SubjectRest>().ReverseMap();
+            });
+
+            Mapper=MapperConfiguration.CreateMapper();
+        }
+
+        public static IMapper GetMapper()
+        {
+            return Mapper;
+        }
+
+        public static IMapper Mapper { get; set; }
+        public static MapperConfiguration MapperConfiguration { get; set; }
+    }
+}
---
+++ StudentAndTeacherApp/App_Start/DIConfig.cs
@@ -0,0 +1,33 @@
+﻿using Autofac;
+using Autofac.Features.ResolveAnything;
+using Autofac.Integration.WebApi;
+using Service.ModuleDI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+using System.Web.Http;
+
+namespace StudentAndTeacherApp.App_Start
+{
+    public class DIConfig
+    {
+        public static void Configure()
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterSource(new AnyConcreteTypeNotAlreadyRegisteredSource());
+            builder.RegisterAssemblyTypes(Assembly.Load("Service"))
+                .Where(t => t.Name.EndsWith("Service"))
+                .AsImplementedInterfaces()
+                .InstancePerLifetimeScope();
+
+            builder.RegisterModule(new RepositoryModuleDI());
+            builder.RegisterModule(new ContextModuleDI());
+           
+            var container=builder.Build();
+            var resolver = new AutofacWebApiDependencyResolver(container);
+            GlobalConfiguration.Configuration.DependencyResolver = resolver;
+        }
+    }
+}
