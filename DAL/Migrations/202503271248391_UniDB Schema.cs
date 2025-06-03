656e5f7e0757f86d707efeeba5e1faf1447ed3ce
KristinaBandic <kristinakiketina@gmail.com>
2025-06-03 13:10:13 +0200

    added autoFac nuget

--- DAL/Migrations/202503271248391_UniDB Schema.cs
+++ DAL/Migrations/202503271248391_UniDB Schema.cs
@@ -5,38 +5,38 @@
     
     public partial class UniDBSchema : DbMigration
     {
-        public override void Up()
-        {
+        public override void Up() {
+            //{
             CreateTable(
                 "dbo.Students",
                 c => new
-                    {
-                        StudentId = c.Int(nullable: false, identity: true),
-                        Name = c.String(),
-                        Age = c.Int(nullable: false),
-                        Class = c.String(),
-                        SubjectSchoolId = c.Int(nullable: false),
-                        TeacherId = c.Int(nullable: false),
-                    })
+                {
+                    StudentId = c.Int(nullable: false, identity: true),
+                    Name = c.String(),
+                    Age = c.Int(nullable: false),
+                    Class = c.String(),
+                    SubjectSchoolId = c.Int(nullable: false),
+                    TeacherId = c.Int(nullable: false),
+                })
                 .PrimaryKey(t => t.StudentId);
-            
+
             CreateTable(
                 "dbo.SubjectSchools",
                 c => new
-                    {
-                        SubjectSchoolId = c.Int(nullable: false, identity: true),
-                        Name = c.String(),
-                    })
+                {
+                    SubjectSchoolId = c.Int(nullable: false, identity: true),
+                    Name = c.String(),
+                })
                 .PrimaryKey(t => t.SubjectSchoolId);
-            
+
             CreateTable(
                 "dbo.Teachers",
                 c => new
-                    {
-                        TeacherId = c.Int(nullable: false, identity: true),
-                        Name = c.String(),
-                        SubjectSchoolId = c.Int(nullable: false),
-                    })
+                {
+                    TeacherId = c.Int(nullable: false, identity: true),
+                    Name = c.String(),
+                    SubjectSchoolId = c.Int(nullable: false),
+                })
                 .PrimaryKey(t => t.TeacherId);
 
             AddForeignKey("dbo.Teachers", "SubjectSchoolId", "dbo.SubjectSchools", "SubjectSchoolId");
